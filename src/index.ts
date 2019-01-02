2ebdd101e58c6f17914d3d6fcaed5bc65f7ab20f
Marc Emmanuel <marcwissler@gmail.com>
2019-01-02 14:59:34 +0100

    Feature/add init (#10)
    * Add init command
    
    * Add option to name folder to init to
    
    * Rename name parameter
    
    * Upgrade generator
    
    * Refactor to ue async await

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import * as commander from 'commander';
 import * as actions from './actions';
+const pjson = require('../package.json');
 
 Object.keys(actions).forEach(key => actions[key](commander));
 
-commander.parse(process.argv);
+commander
+    .version(pjson.version, '-v, --version')
+    .parse(process.argv);
