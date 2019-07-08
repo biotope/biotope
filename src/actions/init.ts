e7d16dc99feefa21f78391325704f5a164af2af8
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2019-07-08 15:48:30 +0200

    feat: 🎸 WIP Check for new version (#29)

--- src/actions/init.ts
+++ src/actions/init.ts
@@ -29,7 +29,7 @@ const npmInstall = async () => {
     npm.on('log', (message) => {
         console.log(message);
     });
-    return new Promise((res, rej) => npm.commands.install(res));
+    return new Promise((res, rej) => npm.commands.install([], res));
 }
 
 const notifySuccess = () => {
